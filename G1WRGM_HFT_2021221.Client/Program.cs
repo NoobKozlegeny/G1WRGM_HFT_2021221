8d2f19b2eae04887689581f42ba7ff07b71aee0b
minec <minec@PAKS>
2021-10-08 19:07:24 +0200

    Add project files.

---
+++ G1WRGM_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace G1WRGM_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
